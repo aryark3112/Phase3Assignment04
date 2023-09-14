dc8b3dcdd0f74ecb8517a8b21a37a31402f541c5
aryark <aryark3112@gmail.com>
2023-09-14 14:40:41 +0530

    adding all

---
+++ Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppAssign04.Models
+{
+    public class Player
+    {
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string UserPwd { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserEmail { get; set; }
+    }
+}
